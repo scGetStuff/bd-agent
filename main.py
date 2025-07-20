b615d981aa07302ac3b744ebe0111a1ac46d5e5d
scGetStuff <67354101+scGetStuff@users.noreply.github.com>
2025-07-20 19:54:49 -0400

    CH1 L3

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+
 def main():
     print("Hello from bd-agent!")
+
+    stuff = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+
+    print(stuff.text)
+    print(f"Prompt tokens: {stuff.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {stuff.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
