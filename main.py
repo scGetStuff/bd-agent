a3c4efdc575fcdb479304c1055a8a44a7fdcf13d
scGetStuff <67354101+scGetStuff@users.noreply.github.com>
2025-07-23 14:36:02 -0400

    CH1 L6

--- main.py
+++ main.py
@@ -8,32 +8,43 @@ from google.genai import types
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+VERBOSE = "--verbose"
 
 
 def main():
     print("Hello from bd-agent!")
 
-    words = readArgs()
+    inputWords, isVerbose = readArgs()
     messages = [
-        types.Content(role="user", parts=[types.Part(text=words)]),
+        types.Content(role="user", parts=[types.Part(text=inputWords)]),
     ]
 
-    stuff = client.models.generate_content(
+    response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
 
-    print(stuff.text)
-    print(f"Prompt tokens: {stuff.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {stuff.usage_metadata.candidates_token_count}")
+    # L6
+    if isVerbose:
+        print(f"User prompt: {inputWords}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    # print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    # print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    # print(response.text)
 
 
-def readArgs():
-    if len(sys.argv) != 2:
-        print('Usage: uv run main.py "a bunch of words"')
+def readArgs() -> (str, bool):
+    if len(sys.argv) < 2:
+        print(f'Usage: uv run main.py "a bunch of words" <{VERBOSE}>')
         sys.exit(1)
 
-    return sys.argv[1]
+    isVerbose = False
+    if len(sys.argv) >= 3:
+        isVerbose = sys.argv[2] == VERBOSE
+
+    return sys.argv[1], isVerbose
 
 
 if __name__ == "__main__":
