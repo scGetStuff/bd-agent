e5035104169e30bc6ed8c8fa4b8c9c730434afd4
scGetStuff <67354101+scGetStuff@users.noreply.github.com>
2025-07-27 19:26:09 -0400

    remove prior lesson code

--- main.py
+++ main.py
@@ -23,28 +23,6 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=inputWords)]),
     ]
-    # CH3 L1
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
-    cfg = types.GenerateContentConfig(system_instruction=system_prompt)
-    # CH3 L2
-    system_prompt = """
-You are a helpful AI coding agent.
-
-When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
-
-- List files and directories
-
-All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
-"""
-    available_functions = types.Tool(
-        function_declarations=[
-            schema_get_files_info,
-        ]
-    )
-    cfg = types.GenerateContentConfig(
-        tools=[available_functions], system_instruction=system_prompt
-    )
-    # CH3 L3
     system_prompt = """
 You are a helpful AI coding agent.
 
@@ -81,9 +59,6 @@ All paths you provide should be relative to the working directory. You do not ne
         print(f"Response tokens: {modelResponse.usage_metadata.candidates_token_count}")
 
     for fun in modelResponse.function_calls:
-        # print(f"Calling function: {fun.name}({fun.args})")
-
-        # CH3 L4
         callContent = call_function(fun, isVerbose)
         funResult = callContent.parts[0].function_response.response
 
